b4db7f3549fa71da2ff2c37f7662a145e411c052
VasudhaBiradar <vasudha.biradar@mitaoe.ac.in>
2024-07-30 20:07:52 +0530

    change

--- src/components/wrappers/App.js
+++ src/components/wrappers/App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import TodoList from '../ui/TodoList';
 import StateProvider from './StateProvider';
 import KeyStrokeHandler from './KeyStrokeHandler';
+import "./App.css";
 
 class App extends Component {
     render() {
